a34aef320a7381bbbe46fa28a30422afaf5dc1fa
Dmytro <savchyndd@gmail.com>
2022-08-12 22:02:32 +0300

    add mobil menu script

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,20 +1,27 @@
-// Scripts mobile, tablet menu
 (() => {
-    const mobileRefs = {
-      mobileMenuBtnRef : document.querySelector("[data-menu-btn]"),
-      mobileMenuRef: document.querySelector("[data-menu]"),
-    };
-  
-    mobileRefs.mobileMenuBtnRef.addEventListener("click", toggleMobileMenu);
-     
-  function toggleMobileMenu() {
-    const expanded =
-      mobileRefs.mobileMenuBtnRef.getAttribute("aria-expanded") === "true" || false;
-      mobileRefs.mobileMenuBtnRef.classList.toggle("is-open");
-      mobileRefs.mobileMenuBtnRef.setAttribute("aria-expanded", !expanded);
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
 
-      mobileRefs.mobileMenuRef.classList.toggle("is-open");
-
-      document.body.classList.toggle("mobile-menu-open");
-    }
-  })();
+  // Закрываем мобильное меню на более широких экранах
+  // в случае изменения ориентации устройства.
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();

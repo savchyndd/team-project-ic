f817348fb91c9ef87968b49dfb7095b0560600ff
Dmytro <savchyndd@gmail.com>
2022-08-14 19:03:01 +0300

    Add fix modal open window, scroll button

--- src/js/modal-order.js
+++ src/js/modal-order.js
@@ -2,15 +2,19 @@
 (() => {
     const refs = {
       openModalOrderBtn: document.querySelector("[data-modal-order-open]"),
+      openMobileModalOrderBtn: document.querySelector("[data-mobile-modal-order-open]"),
       closeModalOrderBtn: document.querySelector("[data-modal-order-close]"),
       modalOrder: document.querySelector("[data-modal-order]"),
     };
   
     refs.openModalOrderBtn.addEventListener("click", toggleModal);
+    refs.openMobileModalOrderBtn.addEventListener("click", toggleModal);
+      
     refs.closeModalOrderBtn.addEventListener("click", toggleModal);
   
-  function toggleModal() {
-    refs.modalOrder.classList.toggle("is-hidden");
-    document.body.classList.toggle("modal-order-open");
+    function toggleModal() {
+      refs.modalOrder.classList.toggle("is-hidden");
+      document.body.classList.toggle("modal-order-open");
+      }
     }
-  })();
+)();

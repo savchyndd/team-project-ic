22d16c36da566689bbddcb5f42667ff3f04f1b40
Dmytro <savchyndd@gmail.com>
2022-08-15 00:34:38 +0300

    fix scroll btn

--- src/js/modal-order.js
+++ src/js/modal-order.js
@@ -1,20 +1,22 @@
 // Script modal order window
 (() => {
-    const refs = {
-      openModalOrderBtn: document.querySelector("[data-modal-order-open]"),
-      openMobileModalOrderBtn: document.querySelector("[data-mobile-modal-order-open]"),
-      closeModalOrderBtn: document.querySelector("[data-modal-order-close]"),
+  var btn = $('.scroll-btn');
+  const refs = {
+    openModalOrderBtn: document.querySelector("[data-modal-order-open]"),
+    openMobileModalOrderBtn: document.querySelector("[data-mobile-modal-order-open]"),
+    closeModalOrderBtn: document.querySelector("[data-modal-order-close]"),
       modalOrder: document.querySelector("[data-modal-order]"),
     };
   
     refs.openModalOrderBtn.addEventListener("click", toggleModal);
     refs.openMobileModalOrderBtn.addEventListener("click", toggleModal);
-      
+    
     refs.closeModalOrderBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
       refs.modalOrder.classList.toggle("is-hidden");
       document.body.classList.toggle("modal-order-open");
+      btn.removeClass('scroll-btn--show');
       }
     }
 )();

293ff03523cc7e86df4ca79eb195cb7d61a77bd6
Dmytro <savchyndd@gmail.com>
2022-08-15 22:38:13 +0300

    fix

--- src/js/slider.js
+++ src/js/slider.js
@@ -2,7 +2,8 @@ $('.review__slider').slick({
   dots: true,
   slidesToShow: 1,
   slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  arrows: false,
 });
 		

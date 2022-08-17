161865e6ad13f58fa403d559260e065463a53d66
Dmytro <savchyndd@gmail.com>
2022-08-17 05:48:11 +0300

    add fix, scroll header

---
+++ src/js/scroll-header.js
@@ -0,0 +1,20 @@
+window.onscroll = function () {
+    myFunction();
+};
+
+var header = document.getElementById('home');
+var headerScroll = $('.header-section');
+var headerPadding = $('.header');
+var headerPaddingPadd = $('.header-padding');
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+      headerScroll.addClass('header__scroll');
+    //   headerPaddingPadd.removeClass('header-padding');
+    //   headerPadding.style.paddingTop = `$10px`;
+  } else {
+      headerScroll.removeClass('header__scroll');
+    //   headerPaddingPadd.addClass('header-padding');
+  }
+}

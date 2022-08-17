12d8180069fb485eac22cfb5bb130a63c93de453
savchyndd <96209694+savchyndd@users.noreply.github.com>
2022-08-17 02:48:54 +0000

    Deploying to gh-pages from @ savchyndd/team-project-ic@161865e6ad13f58fa403d559260e065463a53d66 🚀

---
+++ index.fda1927f.js
@@ -0,0 +1,2 @@
+window.onscroll=function(){myFunction()};var header=document.getElementById("home"),headerScroll=$(".header-section"),headerPadding=$(".header"),headerPaddingPadd=$(".header-padding"),sticky=header.offsetTop;function myFunction(){window.pageYOffset>sticky?headerScroll.addClass("header__scroll"):headerScroll.removeClass("header__scroll")}
+//# sourceMappingURL=index.fda1927f.js.map
